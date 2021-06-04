2c175c8efe45a887b3b3cf4621afd5b426239ccb
latarho <latarho2@gmail.com>
2021-06-04 23:41:33 +0300

    Часть 1. Глава 3. Базовое представление теперь возвращает минимальный HTML.

---
+++ Django_TDD/lists/tests.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+
+from lists.views import home_page
+
+
+class HomePageTest(TestCase):
+    """Тест домашней страницы"""
+
+    def test_root_url_resolves_to_home_page_view(self):
+        """Тест: Корневой url преобразуется в представление домашней страницы"""
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        """Тест: Домашняя страница возвращает правильный html"""
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.strip().startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
---
+++ Django_TDD/lists/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home_page(request):
+    """Домашняя страница"""
+    return render(request, 'home.html')
