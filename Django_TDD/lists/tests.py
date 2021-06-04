5b00b7195dc8c0dd975b77cb9025d87461520e51
latarho <latarho2@gmail.com>
2021-06-05 00:19:17 +0300

    Часть 1. Глава 4. Рефакторинг представления домашней страницы для использования шаблона

--- Django_TDD/lists/tests.py
+++ Django_TDD/lists/tests.py
@@ -1,24 +1,18 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
     """Тест домашней страницы"""
 
-    def test_root_url_resolves_to_home_page_view(self):
-        """Тест: Корневой url преобразуется в представление домашней страницы"""
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
         """Тест: Домашняя страница возвращает правильный html"""
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
         html = response.content.decode('utf8')
-        self.assertTrue(html.strip().startswith('<html>'))
+        self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
 
+    def test_uses_home_template(self):
+        """Тест: Используется шаблон home_page"""
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
