cde90fb459f374842af5a4d4cdc430c8172f79c1
latarho <latarho2@gmail.com>
2021-07-05 22:05:04 +0300

    Часть 1. Глава 5. Сохранение вводимых пользователем данных: тестирование базы данных.

--- Django_TDD/lists/tests.py
+++ Django_TDD/lists/tests.py
@@ -16,3 +16,9 @@ class HomePageTest(TestCase):
         """Тест: Используется шаблон home_page"""
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        """Тест: Можно сохранить post-запрос"""
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
